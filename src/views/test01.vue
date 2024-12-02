d348d696f07e8d8d300078f6b857228f936584b7
Yodarik <yodar@yamaha-motor.co.jp>
2024-12-02 13:58:47 +0900

    これでリリースしたい

---
+++ src/views/test01.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useLockScreen } from 'viy-ui';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const { lockScreen } = useLockScreen();
+
+defineOptions({
+  name: 'test01',
+});
+
+const form = ref();
+
+const formData = reactive({
+});
+</script>
+
+<template>
+  <VueForm ref="form" v-loading="lockScreen" :model="formData">
+    <VueText id="viy2Text_7OqEr" ref="viy2Text_7OqEr" :style="{ width: '100%', display: 'inline-block' }">
+      Hello,World
+    </VueText>
+  </VueForm>
+</template>
